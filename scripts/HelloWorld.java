b4fac38f15c96588655688e87585db4ef008bf28
BRAJESH SINGH <40537685+thakurbrajeshsingh@users.noreply.github.com>
2020-10-27 12:42:07 +0530

    Create HelloWorld.java

---
+++ scripts/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Brajesh Singh
+  GITHUB: https://github.com/thakurbrajeshsingh
+*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
